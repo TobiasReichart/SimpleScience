b2fc6b75de8c2e18ded5be9fee8d07d9b25d5478
Tobias Reichart <tobias-reichart@freenet.de>
2025-09-16 19:55:04 +0200

    Initial commit: Sphinx-Projektstruktur

---
+++ source/conf.py
@@ -0,0 +1,100 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'SimpleScience'
+copyright = '2025, Tobias Reichart'
+author = 'Tobias Reichart'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'de'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_static_path = ['_static']
+
+#
+# -- Meine Anpassungen -------------------------------------------------------
+#
+
+# --- Theme einstellen -------------------------------------------------------
+html_theme = 'sphinx_rtd_theme' # default: 'alabaster'
+
+html_theme_options = {
+'collapse_navigation': False, # Navigationsmenüs nicht einklappen
+'sticky_navigation': False, # Navigation scrollt nicht mit
+'includehidden': True, # hidden toctree einträge werden auch angezeigt
+'prev_next_buttons_location': 'bottom', # bottom (default), top, both, None
+'style_nav_header_background': '#2980B9', # Ändert Hintergrundfarbe der Kopfzeile
+'style_external_links': True, # Externe Links werden gekennzeichnet
+'body_max_width': 'none', # HTM-Seite wird auf auf Bildschirmbreite skalliert
+}
+
+html_show_sourcelink = False # Quelltext-Button ausblenden
+html_css_files = [] # HTML-Styles
+# ----------------------------------------------------------------------------
+
+# --- Umbau von rst auf MySt-Markdown ----------------------------------------
+extensions.append('myst_parser') # Verwendung von MySt-Markdown erlauben
+root_doc = 'index'   # (bei älteren Sphinx-Versionen: master_doc = 'index')
+
+# reST und Markdown parallel erlauben: (Altlasten auch einpfelgbar)
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.md':  'markdown',
+}
+
+# sinnvolle MyST-Erweiterungen
+myst_enable_extensions = [
+    # Liste der MyST-Erweiterungen, die aktiv sind
+    "colon_fence",   # erlaubt ::: fenced directives (Alternative zu ```{note} ... ``` mit :::note)
+    "dollarmath",    # $...$ und $$...$$ für Inline- und Block-Mathe
+    "amsmath",       # unterstützt LaTeX-Umgebungen wie \begin{align}...\end{align}
+    "deflist",       # Definition Lists im Markdown-Stil
+    "tasklist",      # - [ ] und - [x] für Aufgabenlisten mit Checkboxen
+]
+
+# Optionen speziell für die Mathe-Verarbeitung (dollarmath/amsmath)
+extensions.append("sphinx.ext.mathjax") # MathJax für Matheformeln
+myst_dmath_allow_labels = True  # erlaubt \label{} und \ref{} innerhalb von $$...$$
+myst_dmath_allow_space  = True   # erlaubt $x = y$ auch mit Leerzeichen nach dem ersten $
+myst_dmath_allow_digits = True  # erlaubt $3x$ (Ziffer direkt nach $) statt Fehler
+
+# ----------------------------------------------------------------------------
+
+# --- Codeblöcke--------------------------------------------------------------
+extensions.append('sphinx_copybutton') # Kopierbutton für Codeblöcke hinzufügen
+html_css_files.append('codeblock_layout.css') # Anpassen des HTML-Styles der Codeblöcke
+# ----------------------------------------------------------------------------
+
+
+# --- Layout- und Stilmittel -------------------------------------------------
+
+
+
+# Nummerierung von Bidern, Tabellen und Codeblöcken
+numfig = True # Nummerierung von Bildern Tabellen etc. aktivieren
+numfig_format = { # Benennung von Abbildung -> Abb, Tabelle -> Tab, etc. ändern
+    'figure': 'Abb. %s:',    # Bilder: Bsp.: Abb. 1:
+    'table': 'Tab. %s:',     # Tabellen: Bsp.: Tab. 1:
+    'code-block': 'Code %s:' # Codeblöcke: Bsp.: Code 1:
+}
+
+extensions.append("sphinx_design") # Dropdown-Menü hinzufügen
+html_css_files.append('hinweisboxen_layout.css') # Anpassen des HTML-Styles der Hinweisboxen
+
+# --- Downloadbutton ---------------------------------------------------------
+html_css_files.append('download_button.css') # Anpassen des HTML-Styles des Downloadbuttons
+# ----------------------------------------------------------------------------
---
+++ source/index.md
@@ -0,0 +1,25 @@
+# SimpleScience
+
+```{figure} Logo/Logo-black.png
+:name: Logo
+:align: center
+:width: 50%
+```
+Mit SimpleScience versuche ich wissenschaftliche Inhalte verständlichen und praxisnah zu vermitteln und dabei auch meine Methoden für Visualisierung und Dokumentation zu teilen. Der Fokus liegt auf Themen aus Physik, Elektrotechnik, Informatik und Technik, immer mit dem Ziel, den praktischen Nutzen mathematischer Konzepte greifbar zu machen.<br>
+Ich zeige, wofür Mathematik in der Technik tatsächlich gebraucht wird, von komplexen Zahlen in Schwingungen über Differentialgleichungen in der Wellenausbreitung bis hin zu Regelungstechnik und Signalverarbeitung.<br>
+Dabei lege ich großen Wert auf anschauliche Visualisierungen, klare Herleitungen und saubere Darstellungen.
+
+Parallel entwickle ich hier auf der Seite unterstützendes Material:
+- LaTeX-Dokumente
+- Python-Skripte
+- interaktive Plots
+- strukturierte Notizen
+
+Zudem findest du hier auch Tutorials zu meinen eigenen Methoden, mit denen ich diese Inhalte erstelle:<br>
+Wie schreibe ich strukturierte Praktikumsberichte in LaTeX?<br>
+Wie visualisiere ich physikalische Zusammenhänge mit Python?<br>
+Wie baue ich mit Tkinter eine moderne Benutzeroberfläche für eigene Applets?
+
+Wissenschaft kann einfach sein - wenn man sie richtig erklärt.
+
+Dafür stehe ich mit SimpleScience.
